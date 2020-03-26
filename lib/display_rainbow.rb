82130d5e7a6fa7e208b2534d9244ef6bc80b29ee
Kristina Frederick <kristinafrederick@icloud.com>
2020-03-26 03:50:07 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,4 +1,3 @@
 def display_rainbow(colors)
-  colors = "red, orange, yellow, green, blue, indigo, violet"
   puts "R: #{colors[0]}, O: #{colors[1]}, Y: #{[2]}, G: #{[3]}, B: #{[4]}, I: #{[5]}, V: #{[6]}"
 end
